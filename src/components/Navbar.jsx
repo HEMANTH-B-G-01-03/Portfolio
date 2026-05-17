d069481131bb2541771daf49fb4780aad7fc2f03
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-05-17 16:13:37 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Abhishek &nbsp;
-            <span className='sm:block hidden'>Gadhar</span>
+            Hemanth &nbsp;
+            <span className='sm:block hidden'>B G</span>
           </p>
         </Link>
 
