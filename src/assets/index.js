b64d91fb5b0a9f1428f1f58eade68f401e0a826f
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-05-17 15:24:51 +0530

    other remaining images

---
+++ src/assets/index.js
@@ -0,0 +1,83 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import Quiz from "./Quiz.png";
+import Diabetes from "./Diabetes.png";
+import book from "./Bookstore.jpg";
+
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import Expressjs from "./tech/Expressjs.png";
+import Harish from "./Harish.jpg";
+
+import meta from "./company/Air.png";
+import model from "./company/3dmodel.png";
+import shopify from "./company/Disease.png";
+import starbucks from "./company/bookicon.png";
+import tesla from "./company/App.png";
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import restaurant from "./company/restaurant.png";
+import yoga from "./company/yoga.png";
+import urban from "./company/urban.jpg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  Harish,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  model,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  Quiz,
+  Diabetes,
+  book,
+  Expressjs ,
+  restaurant,
+yoga,
+urban, 
+  
+};
